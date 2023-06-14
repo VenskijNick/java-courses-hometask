8670ec180dd7096dfb4e851039fb2ce78864dc2f
VenskijNick <venskijnik@gmail.com>
2023-06-14 19:37:00 +0300

    new lesson

---
+++ lesson3/TimeCheck.java
@@ -0,0 +1,26 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class TimeCheck {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int hours;
+        
+        hours = scan.nextInt();
+        
+        if (hours > 0 && hours <= 4){
+            System.out.println("ночь");
+        } else if (hours> 4 && hours <= 12) {
+            System.out.println("утро");
+            
+        } else if (hours > 12 && hours <= 18) {
+            System.out.println("день");
+            
+        } else if (hours>18 && hours <23) {
+            System.out.println("вечер");
+
+        }
+
+    }
+}
