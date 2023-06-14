718436b21d3bb321c636c7ecd5212ce7ea96ec1e
VenskijNick <venskijnik@gmail.com>
2023-06-14 19:59:47 +0300

    new lesson

--- lesson3/SwitchTime.java
+++ lesson3/SwitchTime.java
@@ -9,20 +9,17 @@ public class SwitchTime {
         String time = scan.next();
 
         switch (time) {
-            case "morning":
+            case "morning" -> {
                 System.out.println("Morning starts from 6 am to 12 am");
-                break;
-            case "Day":
-                System.out.println("Day starts from 12 am to 6 pm");
-                break;
-            case "evening":
-                System.out.println("Evening starts from 6 pm to 11 pm");
-                break;
-            case "night":
-                System.out.println("Night starts from 11 pm to 6 am");
-                break;
-            default:
-                System.out.println("Unknown time");
+            }
+
+            case "Day" -> System.out.println("Day starts from 12 am to 6 pm");
+
+            case "evening" -> System.out.println("Evening starts from 6 pm to 11 pm");
+
+            case "night" -> System.out.println("Night starts from 11 pm to 6 am");
+
+            default -> System.out.println("Unknown time");
         }
 
     }
