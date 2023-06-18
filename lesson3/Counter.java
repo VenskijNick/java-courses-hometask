06ed007fcd0ae76c699233bae88df5df87c18543
VenskijNick <venskijnik@gmail.com>
2023-06-18 12:48:47 +0300

    new hometask

--- lesson3/Counter.java
+++
@@ -1,9 +0,0 @@
-package lesson3;
-
-public class Counter {
-    public static void main(String[] args) {
-        for (int i = 100; i!=120;i-=10){
-            System.out.println(i);
-        }
-    }
-}
